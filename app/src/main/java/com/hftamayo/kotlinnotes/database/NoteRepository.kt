750ca7c25b5e9d0972303d0fa0d5c87ee40d50e0
hftamayo <hftamayo@gmail.com>
2023-06-01 09:00:09 -0600

    adding function related to data persistence

--- app/src/main/java/com/hftamayo/kotlinnotes/database/NoteRepository.kt
+++ app/src/main/java/com/hftamayo/kotlinnotes/database/NoteRepository.kt
@@ -5,4 +5,16 @@ import com.hftamayo.kotlinnotes.models.Note
 
 class NoteRepository(private val noteDao: NoteDao) {
     val allNotes : LiveData<List<Note>> = noteDao.getAllNotes()
+
+    suspend fun insert(note: Note){
+        noteDao.insert(note)
+    }
+
+    suspend fun delete(note: Note){
+        noteDao.delete(note)
+    }
+
+    suspend fun update(note: Note){
+        noteDao.update(note.id, note.title, note.note)
+    }
 }
