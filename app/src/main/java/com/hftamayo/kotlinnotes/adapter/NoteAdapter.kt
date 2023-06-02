48b5b96fa37608f88e39126954a96ce9d2360b3b
hftamayo <hftamayo@gmail.com>
2023-06-02 08:35:10 -0600

    working on diff methods from Adapter class

---
+++ app/src/main/java/com/hftamayo/kotlinnotes/adapter/NoteAdapter.kt
@@ -0,0 +1,44 @@
+package com.hftamayo.kotlinnotes.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.hftamayo.kotlinnotes.R
+import com.hftamayo.kotlinnotes.models.Note
+
+class NoteAdapter(private val context: Context) : RecyclerView.Adapter<NoteAdapter.NoteViewHolder> {
+
+    private val NotesList : ArrayList<Note>()
+    private val fullList : ArrayList<Note>()
+
+    inner class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val notes_layout = itemView.findViewById<CardView>(R.id.card_layout)
+        val title = itemView.findViewById<TextView>(R.id.tv_title)
+        val Note_tv = itemView.findViewById<TextView>(R.id.tv_note)
+        val date = itemView.findViewById<TextView>(R.id.tv_date)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        return NoteViewHolder(
+            LayoutInflater.from(context).inflate(R.layout.list_item, parent, false)
+        )
+
+    }
+
+    override fun getItemCount(): Int {
+        return NotesList.size
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val currentNote = NotesList[position]
+        holder.title.text = currentNote.title
+        holder.title.isSelected = true
+        holder.Note_tv.text = currentNote.title
+        holder.date.text = currentNote.date
+    }
+}
