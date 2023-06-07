4d2c645fec27bf92079f439e8ee93a3317575439
hftamayo <hftamayo@gmail.com>
2023-06-07 11:16:47 -0600

    methods for iteration of the object's items part1

--- app/src/main/java/com/hftamayo/kotlinnotes/MainActivity.kt
+++ app/src/main/java/com/hftamayo/kotlinnotes/MainActivity.kt
@@ -2,10 +2,37 @@ package com.hftamayo.kotlinnotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Note
+import androidx.lifecycle.ViewModelProvider
+import com.hftamayo.kotlinnotes.adapter.NoteAdapter
+import com.hftamayo.kotlinnotes.database.NoteDatabase
+import com.hftamayo.kotlinnotes.databinding.ActivityMainBinding
+import com.hftamayo.kotlinnotes.models.NoteViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var database : NoteDatabase
+    lateinit var viewModel : NoteViewModel
+    lateinit var adapter : NoteAdapter
+    lateinit var selectedNote : Note
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initUI()
+        viewModel = ViewModelProvider(this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(application)).get(NoteViewModel::class.java)
+
+        viewModel.allnotes.observe(this) { list ->
+            list?.let {
+                adapter.updateList(list)
+            }
+        }
+    }
+
+    private fun initUI(){
+
     }
 }
