1246355f28bceaa132dd4663fde1214420efdb22
hftamayo <hftamayo@gmail.com>
2023-06-08 08:41:20 -0600

    first version of Note View Model

--- app/src/main/java/com/hftamayo/kotlinnotes/models/NoteViewModel.kt
+++ app/src/main/java/com/hftamayo/kotlinnotes/models/NoteViewModel.kt
@@ -1,4 +1,28 @@
 package com.hftamayo.kotlinnotes.models
 
-class NoteViewModel {
+import android.app.Application
+import com.hftamayo.kotlinnotes.database.NoteDatabase
+import com.hftamayo.kotlinnotes.database.NoteRepository
+
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository : NoteRepository
+    val allnotes : LiveData<List<Note>>
+
+    init {
+        val dao = NoteDatabase.getDatabase(application).getNoteDao()
+        repository = NoteRepository(dao)
+        allnotes = repository.allNotes
+    }
+
+    fun deleteNote(note : Note) = viewModelScope.launch(Dispatchers.IO){
+        repository.delete(note)
+    }
+
+    fun insertNote(note : Note) = viewModelScope.launch(Dispatchers.IO){
+        repository.insert(note)
+    }
+
+    fun updateNote(note : Note) = viewModelScope.launch(Dispatchers.IO){
+        repository.update(note)
+    }
 }
