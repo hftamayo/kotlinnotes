39e48a3d70962954a529bfff0d50e884d6f17c24
hftamayo <hftamayo@gmail.com>
2023-06-19 08:53:03 -0600

    method for setting data entered by the user

--- app/src/main/java/com/hftamayo/kotlinnotes/AddNote.kt
+++ app/src/main/java/com/hftamayo/kotlinnotes/AddNote.kt
@@ -1,18 +1,26 @@
 package com.hftamayo.kotlinnotes;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
 import com.hftamayo.kotlinnotes.databinding.ActivityAddNoteBinding;
+import com.hftamayo.kotlinnotes.models.Note
 
 class AddNote : AppCompatActivity() {
     private lateinit var binding: ActivityAddNoteBinding
+    private lateinit var note : Note
+    private lateinit var old_note : Note
+    var isUpdate = false
 
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState);
 
         binding = ActivityAddNoteBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_add_note);
+        setContentView(binding.root);
+        try{
+            old_note = intent.getSerializableExtra("current_note") as Note
+            binding.etTitle.setText(old_note.title)
+            binding.etNote.setText(old_note.note)
+            isUpdate = true
+        }
     }
 }
